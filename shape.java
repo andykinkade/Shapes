ca5110bd6451e71ad9ed1e79adff06ed6c16a587
andykinkade <c3380903@uon.edu.au>
2024-04-02 13:37:25 +1100

    First initial making of shape
    Just making the class and importing class and defualt constructor

---
+++ shape.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class shape {
+
+	private int sides;
+	private float area;
+	private String type;
+
+	public shape() {
+		this.sides = 0;
+		this.area = 0.0;
+		this.type ="";
+	}
+}
